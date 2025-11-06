f1145caf64dac9c2c2d433987e05add301bfa301
danielmmerino <danielm_merino@hotmail.com>
2025-11-05 20:00:46 -0500

    Add QR reception page for agenda lookup

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/recepcion/agenda', 'agenda.recepcion')->name('recepcion.agenda');
